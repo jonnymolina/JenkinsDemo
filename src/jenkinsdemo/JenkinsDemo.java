b95ad098f6e6384eef6e096da00cfb975f676170
Jonathan Molina <nemolina@calpoly.edu>
2016-10-31 16:48:03 -0700

    initial commit

---
+++ src/jenkinsdemo/JenkinsDemo.java
@@ -0,0 +1,16 @@
+package jenkinsdemo;
+
+/**
+ *
+ * @author Jonathan Molina
+ */
+public class JenkinsDemo
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
